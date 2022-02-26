c1b8833d4d2e5f2f1b16f8733a2200b0cef6833c
rishabh0071 <57853470+rishabh0071@users.noreply.github.com>
2022-02-26 16:00:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var b=['sounds/crash.mp3','sounds/kick-bass.mp3','sounds/snare.mp3','sounds/tom-1.mp3','sounds/tom-2.mp3','sounds/tom-3.mp3','sounds/tom-4.mp3',];
+
+
+
+function handDrum(id){
+    var audio = new Audio(b[id]);
+    audio.play();
+}
+
+
+
